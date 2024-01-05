a69cca7d25d9d9a1055a32bbe64a1d4af909644a
Khaled <Khaled195341@feng.bu.edu.eg>
2024-01-05 20:30:58 +0200

    cors problem

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import colors from "colors"
+import { Server } from "socket.io"
+import http from "http"
+import cors from "cors"
 import userRouter from "./routes/user.js"
 import chatRouter from "./routes/chat.js"
 import { notFound, errorHandler } from "./middleware/errorHandler.js"
@@ -9,20 +12,33 @@ import protect from "./middleware/autherization.js"
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
+const corsOptions = {
+	origin: process.env.FRONT_END_URL,
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	credentials: true, // enable set cookie
+	optionsSuccessStatus: 204,
+}
+
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+	cors: corsOptions,
+})
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use("/api/user/", userRouter)
 app.use("/api/chats/", protect, chatRouter)
 app.use(notFound)
 app.use(errorHandler)
 
-
 app.get("/", (req, res) => {
 	res.send("testing...")
 })
 
 const start = async () => {
-	app.listen(PORT, () => console.log(`server listening on ${PORT} ... `.blue.bold))
+	server.listen(PORT, () =>
+		console.log(`server listening on ${PORT} ... `.blue.bold)
+	)
 	await connectDB()
 }
 
